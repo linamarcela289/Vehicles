aca89907e6fff4807337f570912429471b5a5a76
linamarcela289 <linagaleano0@gmail.com>
2021-08-27 09:50:58 -0500

    Add table Vehicles

---
+++ Web.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.API.Data.Entities;
+
+namespace Web.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<VehicleType> vehicleTypes { get; set; }
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
---
+++ Web.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.API.Data.Entities
+{
+    public class VehicleType
+
+    {
+        public int Id { get; set; }
+        [Display(Name = "Tipo de vehiculo")]
+        [MaxLength(20, ErrorMessage = "El campo {0} no puede tener mas de {1} caracter")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Description { get; set; }
+
+    }
+}
